6e852114ae94da013a433893832ecd34cd571d51
Isaac <faceless.shop@gmail.com>
2012-02-28 20:28:44 -0700

    OS specific settings
    -Add OS specific folders and settings files etc.
    -Add simple plugin to copy OS specific settings

---
+++ os_specific_user_files.py
@@ -0,0 +1,84 @@
+import sublime
+import sublime_plugin
+import os
+import shutil
+import threading
+
+ossettings = sublime.load_settings('os_specific_user_files.sublime-settings')
+osplatform = sublime.platform()
+user = os.path.join(sublime.packages_path(), 'User')
+ospath = os.path.join(user, 'OsSpecificUserFiles', osplatform)
+running_thread = False
+
+
+def run_copy_thread(force=False):
+    if not running_thread:
+        t = CopyOsUserFiles(ossettings.get(osplatform, {}), force)
+        t.start()
+
+
+def copy_file(src, dest):
+    try:
+        shutil.copyfile(src, dest)
+    except:
+        print "Could not copy file: %d" % src
+
+
+def copy_directory(src, dest):
+    try:
+        if os.path.exists(dest):
+            print "Directory already exists: %s" % dest
+            print "Removing directory before copy"
+            shutil.rmtree(dest)
+    except:
+        print "Could not remove: %s" % dest
+        return
+
+    try:
+        shutil.copytree(src, dest)
+    except:
+        print "Could not copy directory: %d" % src
+
+
+class CopyOsUserFiles(threading.Thread):
+    def __init__(self, file_list, force=False):
+        self.force = force
+        self.file_list = file_list
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global running_thread
+        running_thread = True
+        self.copy_all()
+        running_thread = False
+
+    def copy_all(self):
+        # Copy single files
+        for item in self.file_list['files']:
+            key = item
+            value = self.file_list['files'][item]
+            src = os.path.join(ospath, key)
+            dest_dir = os.path.join(user, os.path.dirname(value))
+            dest = os.path.join(dest_dir, key)
+
+            if (not os.path.exists(dest) or self.force) and os.path.exists(src) and os.path.exists(dest_dir):
+                copy_file(src, dest)
+
+        # Copy directories
+        for item in self.file_list['directories']:
+            key = item
+            value = self.file_list['directories'][item]
+            src = os.path.join(ospath, key)
+            dest_dir = os.path.join(user, os.path.dirname(value))
+            dest = os.path.join(dest_dir, key)
+
+            if (not os.path.exists(dest) or self.force) and os.path.exists(src) and os.path.exists(dest_dir):
+                copy_directory(src, dest)
+
+
+class CopyOsUserFilesCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        run_copy_thread(force=True)
+
+
+run_copy_thread(force=False)
