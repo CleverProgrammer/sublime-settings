08b430e70390d0346ff91375efb1145b1af51db0
facelessuser <faceless.shop@gmail.com>
2012-03-09 18:13:39 -0700

    Update settings and such

--- sort_tabs.py
+++ sort_tabs.py
@@ -1,26 +1,24 @@
 import sublime_plugin
-import os
+from os import path
 from operator import itemgetter
 
 
-class SortTabsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+class SortTabsCommand(sublime_plugin.WindowCommand):
+    def run(self):
         file_views = []
-        win = self.view.window()
+        win = self.window
         curr_view = win.active_view()
         for vw in win.views():
-            head, tail = os.path.split(vw.file_name())
+            _, tail = path.split(vw.file_name() or path.sep)
             group, _ = win.get_view_index(vw)
             file_views.append((tail.lower(), vw, group))
         file_views.sort(key=itemgetter(2, 0))
         for index, (_, vw, group) in enumerate(file_views):
-            if not index:
-                prev_group = group
+            print "true" if "prev_group" in locals() else "flase"
+            if index == 0 or group > prev_group:
                 moving_index = 0
-            elif group > prev_group:
-                    moving_index = 0
-                    prev_group = group
+                prev_group = group
             else:
                 moving_index += 1
-            win.set_view_index(vw, group, moving_index)
+                win.set_view_index(vw, group, moving_index)
         win.focus_view(curr_view)
