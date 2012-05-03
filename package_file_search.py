22f8318e02b6f83c73c4ffc58ff2f51cd16d200e
facelessuser <faceless.shop@gmail.com>
2012-05-03 10:55:54 -0600

    -Add regex search for packge file search plugin -Allow turning off deep_search via the input panel in package file search plugin

--- package_file_search.py
+++ package_file_search.py
@@ -9,32 +9,50 @@ import sublime_plugin
 from os.path import join, isdir, normpath
 from os import listdir, walk
 from fnmatch import fnmatch
+import re
 
 
 DEFAULT_FILES = [
-    {"caption": "Settings Files",        "search": {"pattern": "*.sublime-settings", "deep_search": True}},
-    {"caption": "Keymap Files",          "search": {"pattern": "*.sublime-keymap",   "deep_search": True}},
-    {"caption": "Command Files",         "search": {"pattern": "*.sublime-commands", "deep_search": True}},
-    {"caption": "Readme Files",          "search": {"pattern": "*readme*",           "deep_search": True}},
-    {"caption": "Language Syntax Files", "search": {"pattern": "*tmLanguage",        "deep_search": True}},
-    {"caption": "Snippet Files",         "search": {"pattern": "*.sublime-snippet",  "deep_search": True}},
-    {"caption": "Preference Files",      "search": {"pattern": "*.tmPreferences",    "deep_search": True}},
-    {"caption": "Color Scheme Files",    "search": {"pattern": "*.tmTheme",          "deep_search": True}},
-    {"caption": "Theme Files",           "search": {"pattern": "*.sublime-theme",    "deep_search": True}},
-    {"caption": "Python Source Files",   "search": {"pattern": "*.py",               "deep_search": True}}
+    {"caption": "Settings Files",        "search": {"pattern": "*.sublime-settings", "deep_search": True, "regex": False}},
+    {"caption": "Keymap Files",          "search": {"pattern": "*.sublime-keymap",   "deep_search": True, "regex": False}},
+    {"caption": "Command Files",         "search": {"pattern": "*.sublime-commands", "deep_search": True, "regex": False}},
+    {"caption": "Readme Files",          "search": {"pattern": "*readme*",           "deep_search": True, "regex": False}},
+    {"caption": "Language Syntax Files", "search": {"pattern": "*tmLanguage",        "deep_search": True, "regex": False}},
+    {"caption": "Snippet Files",         "search": {"pattern": "*.sublime-snippet",  "deep_search": True, "regex": False}},
+    {"caption": "Preference Files",      "search": {"pattern": "*.tmPreferences",    "deep_search": True, "regex": False}},
+    {"caption": "Color Scheme Files",    "search": {"pattern": "*.tmTheme",          "deep_search": True, "regex": False}},
+    {"caption": "Theme Files",           "search": {"pattern": "*.sublime-theme",    "deep_search": True, "regex": False}},
+    {"caption": "Python Source Files",   "search": {"pattern": "*.py",               "deep_search": True, "regex": False}}
 ]
 
 
 class GetPackageFilesInputCommand(sublime_plugin.WindowCommand):
-    def find_pattern(self, pattern, deep_search):
+    def find_pattern(self, pattern):
+        regex = False
+        deep_search = True
         if pattern != "":
-            self.window.run_command("get_package_files", {"pattern": pattern, "deep_search": deep_search})
+            m = re.match(r"^\[deep_search=(true|false)\](.*)", pattern)
+            if m != None:
+                deep_search = True if m.group(1) == "true" else False
+                pattern = m.group(2)
+            m = re.match(r"^[ \t]*`(.*)`[ \t]*$", pattern)
+            if m != None:
+                regex = True
+                pattern = m.group(1)
+            self.window.run_command(
+                "get_package_files",
+                {
+                    "pattern": pattern,
+                    "deep_search": deep_search,
+                    "regex": regex
+                }
+            )
 
-    def run(self, deep_search=True):
+    def run(self):
         self.window.show_input_panel(
             "File Pattern: ",
             "",
-            lambda x: self.find_pattern(x, deep_search=deep_search),
+            self.find_pattern,
             None,
             None
         )
@@ -44,7 +62,14 @@ class GetPackageFilesMenuCommand(sublime_plugin.WindowCommand):
     def find_files(self, value, patterns):
         if value > -1:
             pat = patterns[value]
-            self.window.run_command("get_package_files", {"pattern": pat["pattern"], "deep_search": pat["deep_search"]})
+            self.window.run_command(
+                "get_package_files",
+                {
+                    "pattern": pat["pattern"],
+                    "deep_search": pat["deep_search"],
+                    "regex": pat["regex"]
+                }
+            )
 
     def run(self, pattern_list=DEFAULT_FILES):
         patterns = []
@@ -59,30 +84,34 @@ class GetPackageFilesMenuCommand(sublime_plugin.WindowCommand):
 
 
 class GetPackageFilesCommand(sublime_plugin.WindowCommand):
-    def find_files(self, files, pattern, settings, base=None):
+    def find_files(self, files, pattern, settings, regex):
         for f in files:
-            if fnmatch(f, pattern):
-                name = f if base == None else join(base, f)
-                settings.append(name.replace(self.packages, "").lstrip("\\").lstrip("/"))
+            if regex:
+                if re.match(pattern, f, re.IGNORECASE) != None:
+                    settings.append(f.replace(self.packages, "").lstrip("\\").lstrip("/"))
+            else:
+                if fnmatch(f, pattern):
+                    settings.append(f.replace(self.packages, "").lstrip("\\").lstrip("/"))
 
-    def walk(self, settings, plugin, pattern, deep_search=True):
+    def walk(self, settings, plugin, pattern, deep_search=True, regex=False):
         if deep_search:
             for base, dirs, files in walk(plugin):
-                self.find_files(files, pattern, settings, base)
+                files = [join(base, f) for f in files]
+                self.find_files(files, pattern, settings, regex)
         else:
             files = [join(plugin, item) for item in listdir(plugin) if not isdir(join(plugin, item))]
-            self.find_files(files, pattern, settings)
+            self.find_files(files, pattern, settings, regex)
 
     def open_file(self, value, settings):
         if value > -1:
             self.window.open_file(join(self.packages, settings[value]))
 
-    def run(self, pattern, deep_search=True):
+    def run(self, pattern, deep_search=True, regex=False):
         self.packages = normpath(sublime.packages_path())
         settings = []
         plugins = [join(self.packages, item) for item in listdir(self.packages) if isdir(join(self.packages, item))]
         for plugin in plugins:
-            self.walk(settings, plugin, pattern, deep_search)
+            self.walk(settings, plugin, pattern.strip(), deep_search, regex)
         self.window.show_quick_panel(
             settings,
             lambda x: self.open_file(x, settings=settings)
