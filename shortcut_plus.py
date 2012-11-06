a8aa09dfa261d04e9e6f440a6dc6d843892203f4
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2012-11-06 13:24:33 -0700

    -ShortcutPlus plugin added

---
+++ shortcut_plus.py
@@ -0,0 +1,120 @@
+"""
+Shortcut Plus
+
+Copyright (c) 2012 Isaac Muse <isaacmuse@gmail.com>
+License: MIT
+
+Example:  This shows how to define two shortcut profiles bound to shortcuts
+
+    // Shortcut Plus Toggle
+    {
+        "keys": ["alt+`"],
+        "command": "toggle_shortcut_plus",
+        "context": [{"key": "toggle_shortcut_plus"}],
+        "args": {"profile": "MyProfile1"}
+    },
+    // Shortcut Plus Toggle
+    {
+        "keys": ["ctrl+alt+`"],
+        "command": "toggle_shortcut_plus",
+        "context": [{"key": "toggle_shortcut_plus"}],
+        "args": {"profile": "MyProfile2"}
+    },
+
+Example: This shows how to create shortcuts that execute only in a given shorcut profile
+         The first is bound to Myprofile1 and shows a dialog when all selections are empty
+         and the escape key is pressed.
+
+         The other is bound to MyProfile2 and shows the inverse when escape is pressed.
+
+         "shortcut_plus_test" is command only for testing.  You can use any command you want.
+
+    // Shortcut Plus Test
+    {
+        "keys": ["escape"],
+        "command": "shortcut_plus",
+        "context":
+        [
+            {"key": "shortcut_plus:MyProfile1"},
+            {"key": "selection_empty", "operator": "equal", "operand": true, "match_all": true}
+        ],
+        "args": {
+            "command_type": "window",
+            "command": "shortcut_plus_test",
+            "args": {
+                "msg": "All selection are empty!"
+            }
+        }
+    },
+    // Shortcut Plus Test
+    {
+        "keys": ["escape"],
+        "command": "shortcut_plus",
+        "context":
+        [
+            {"key": "shortcut_plus:MyProfile2"},
+            {"key": "selection_empty", "operator": "equal", "operand": false, "match_all": true}
+        ],
+        "args": {
+            "command_type": "window",
+            "command": "shortcut_plus_test",
+            "args": {
+                "msg": "All selections are not empty!"
+            }
+        }
+    }
+"""
+
+import sublime
+import sublime_plugin
+
+
+class ShortcutMode(object):
+    enabled = False
+    profile = ""
+
+
+class ShortcutPlusModeListener(sublime_plugin.EventListener):
+    def on_query_context(self, view, key, operator, operand, match_all):
+        handeled = False
+        if key == "toggle_shortcut_plus":
+            handeled = True
+        elif ShortcutMode.enabled and key.startswith("shortcut_plus:"):
+            if ShortcutMode.profile == key[len("shortcut_plus:"):len(key)]:
+                handeled = True
+        return handeled
+
+
+class ShortcutPlusCommand(sublime_plugin.ApplicationCommand):
+    def run(self, command_type, command, args):
+        if command_type == "application":
+            sublime.run_command(command, args)
+        else:
+            window = sublime.active_window()
+            if window is not None:
+                if command_type == "window":
+                    window.run_command(command, args)
+                else:
+                    view = window.active_view()
+                    if view is not None:
+                        if command_type == "text":
+                            view.run_command(command, args)
+
+
+class ToggleShortcutPlusCommand(sublime_plugin.ApplicationCommand):
+    def run(self, profile):
+        if profile == "" or ShortcutMode.profile == profile:
+            ShortcutMode.enabled = False
+            ShortcutMode.profile = ""
+        elif profile != ShortcutMode.profile:
+            ShortcutMode.enabled = True
+            ShortcutMode.profile = profile
+        msg = "Shortcut Plus: %s" % (
+            "%s Enabled" % ShortcutMode.profile if ShortcutMode.enabled else "Disabled"
+        )
+        sublime.status_message(msg)
+
+
+class ShortcutPlusTestCommand(sublime_plugin.WindowCommand):
+    def run(self, msg):
+        sublime.message_dialog(msg)
