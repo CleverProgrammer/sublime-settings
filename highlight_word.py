a732af255382f2d2cd7cb94f38e61f0dcad9d319
Isaac Muse <faceless.shop@gmail.com>
2013-02-15 16:00:52 -0700

    Update highlight_word.py
    Quick change that allows the thread to restart if changes are made to the script (could possibly be done a little cleaner in the future)

--- highlight_word.py
+++ highlight_word.py
@@ -1,5 +1,7 @@
 import sublime
 import sublime_plugin
+from time import time, sleep
+import _thread as thread
 
 KEY = "HighlightCurrentWord"
 STYLE = "solid"
@@ -12,22 +14,10 @@ def debug(s):
 
 # The search is performed half a second after the most recent event in order to prevent the search hapenning on every keypress.
 # Each of the event handlers simply marks the time of the most recent event and a timer periodically executes doSearch
-class HighlightCurrentWord(sublime_plugin.EventListener):
+class HighlightWord(object):
     def __init__(self):
         self.previousRegion = sublime.Region(0, 0)
-        sublime.set_timeout(self.on_timer, 50)
         self.highlighting = False
-        super().__init__()
-
-    def on_timer(self):
-        sublime.set_timeout(self.on_timer, 50)
-        window = sublime.active_window()
-        view = window.active_view() if window != None else None
-        if not self.highlighting:
-            try:
-                self.doSearch(view, False)
-            except:
-                self.highlighting = False
 
     def doSearch(self, view, force=True):
         self.highlighting = True
@@ -140,9 +130,69 @@ class HighlightCurrentWord(sublime_plugin.EventListener):
 
         self.highlighting = False
 
+
+class HwEventManager(object):
+    @classmethod
+    def load(cls):
+        cls.wait_time = 0.12
+        cls.time = time()
+        cls.modified = False
+        cls.ignore_all = False
+
+HwEventManager.load()
+
+
+class HwThreadMgr(object):
+    restart = False
+
+
+class HighlightWordListenerCommand(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
-        if not self.highlighting:
-            try:
-                self.doSearch(view)
-            except:
-                self.highlighting = False
+        if HwEventManager.ignore_all:
+            return
+        now = time()
+        if now - HwEventManager.time > HwEventManager.wait_time:
+            sublime.set_timeout(lambda: hw_run(), 0)
+        else:
+            HwEventManager.modified = True
+            HwEventManager.time = now
+
+
+# Kick off hex highlighting
+def hw_run():
+    HwEventManager.modified = False
+    # Ignore selection and edit events inside the routine
+    HwEventManager.ignore_all = True
+    highlight_word(sublime.active_window().active_view())
+    HwEventManager.ignore_all = False
+    HwEventManager.time = time()
+
+
+# Start thread that will ensure highlighting happens after a barage of events
+# Initial highlight is instant, but subsequent events in close succession will
+# be ignored and then accounted for with one match by this thread
+def hw_loop():
+    while not HwThreadMgr.restart:
+        if HwEventManager.modified == True and time() - HwEventManager.time > HwEventManager.wait_time:
+            sublime.set_timeout(lambda: hw_run(), 0)
+        elif not HwEventManager.modified:
+            sublime.set_timeout(lambda: hw_run(), 0)
+        sleep(0.5)
+
+    if HwThreadMgr.restart:
+        HwThreadMgr.restart = False
+        sublime.set_timeout(lambda: thread.start_new_thread(hw_loop, ()), 0)
+
+
+def plugin_loaded():
+    global highlight_word
+    highlight_word = HighlightWord().doSearch
+
+    if not 'running_hw_loop' in globals():
+        global running_hw_loop
+        running_hw_loop = True
+        debug("Start Thread")
+        thread.start_new_thread(hw_loop, ())
+    else:
+        HwThreadMgr.restart = True
+        debug("Restart Thread")
