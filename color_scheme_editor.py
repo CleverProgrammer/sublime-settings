3976e5bdd52abe3d872b8354e146acccc5958717
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2013-04-01 16:24:07 -0600

    -update settings

--- color_scheme_editor.py
+++ color_scheme_editor.py
@@ -3,13 +3,15 @@ import sublime_plugin
 from os.path import join, exists, basename, normpath, dirname
 from os import makedirs
 import subprocess
-from os import popen as call
 
 PLUGIN_NAME = "User"
+THEME_EDITOR = None
 
 
 class ColorSchemeEditorCommand(sublime_plugin.ApplicationCommand):
-    def run(self):
+    def run(self, no_direct_edit=True):
+        if THEME_EDITOR is None:
+            return
         # Get current color scheme
         settings = sublime.load_settings('Preferences.sublime-settings')
         scheme_file = settings.get('color_scheme', None)
@@ -18,7 +20,10 @@ class ColorSchemeEditorCommand(sublime_plugin.ApplicationCommand):
         actual_scheme_file = join(dirname(sublime.packages_path()), normpath(scheme_file))
         if scheme_file is not None:
             # If scheme cannot be found, it is most likely in an archived package
-            if not exists(actual_scheme_file):
+            if (
+                not exists(actual_scheme_file) or
+                (no_direct_edit and not scheme_file.startswith("Packages/User/ColorSchemeEditorTemp"))
+            ):
                 # Create temp folder
                 zipped_themes = join(sublime.packages_path(), "User", "ColorSchemeEditorTemp")
                 if not exists(zipped_themes):
@@ -27,8 +32,12 @@ class ColorSchemeEditorCommand(sublime_plugin.ApplicationCommand):
                 # Read theme file into memory and write out to the temp directory
                 text = sublime.load_binary_resource(scheme_file)
                 actual_scheme_file = join(zipped_themes, basename(scheme_file))
-                with open(actual_scheme_file, "wb") as f:
-                    f.write(text)
+                try:
+                    with open(actual_scheme_file, "wb") as f:
+                        f.write(text)
+                except:
+                    sublime.error_message("Color Scheme Editor:\nCould not copy theme file to temp directory.")
+                    return
 
                 # Load unarchived theme
                 scheme_file = settings.set('color_scheme', "Packages/User/ColorSchemeEditorTemp/%s" % basename(scheme_file))
