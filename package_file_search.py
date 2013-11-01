09dd7481f0d01e9a3ead65b89a09f70a831a7239
facelessuser <faceless.shop@gmail.com>
2013-10-31 22:07:04 -0600

    Refactor classes

--- package_file_search.py
+++ package_file_search.py
@@ -142,48 +142,16 @@ class GetPackageFilesMenuCommand(sublime_plugin.WindowCommand):
                 lambda x: self.find_files(x, patterns=patterns, find_all=find_all)
             )
 
-class GetColorSchemeFileCommand(GetPackageFilesCommand):
-    def preview_color(self, value, settings):
-        if value != -1:
-            sublime.load_settings("Preferences.sublime-settings").set("color_scheme", settings[value])
 
-    def set_color_scheme(self, value, settings):
-        if value != -1:
-            sublime.load_settings("Preferences.sublime-settings").set("color_scheme", settings[value])
-        else:
-            if self.current_color_scheme is not None:
-                sublime.load_settings("Preferences.sublime-settings").set("color_scheme", self.current_color_scheme)
+class _PackageSearch(sublime_plugin.WindowCommand):
+    def pre_process(self, **kwargs):
+        return kwargs
 
-    def find(self, pattern, regex):
-        resources = []
-        if not regex:
-            resources = sublime.find_resources(pattern)
-        else:
-            temp = sublime.find_resources("*")
-            for t in temp:
-                if re.match(pattern, t, re.IGNORECASE) != None:
-                    resources.append(t)
+    def on_select(self, value, settings):
+        pass
 
-        self.window.show_quick_panel(
-            resources,
-            lambda x: self.set_color_scheme(x, settings=resources),
-            0,
-            0,
-            lambda x: self.preview_color(x, settings=resources)
-        )
-
-    def run(self, pattern=None, regex=False, find_all=False):
-        pattern = "*.tmTheme"
-        regex = False
-        find_all = False
-        self.current_color_scheme = sublime.load_settings("Preferences.sublime-settings").get("color_scheme")
-        self.find(pattern, regex)
-
-
-class GetPackageFilesCommand(sublime_plugin.WindowCommand):
-    def open_file_quick(self, value, settings):
-        if value > -1:
-            self.window.run_command("open_file", {"file": settings[value].replace("Packages", "${packages}", 1)})
+    def process_file(self, value, settings):
+        pass
 
     def find_files(self, files, pattern, settings, regex):
         for f in files:
@@ -199,13 +167,71 @@ class GetPackageFilesCommand(sublime_plugin.WindowCommand):
             files = [(join(base, f), "Packages") for f in files]
             self.find_files(files, pattern, settings, regex)
 
-    def open_file(self, value, settings):
-        if value > -1:
-            if value >= self.zipped_idx:
-                self.open_zip_file(settings[value][0])
-            else:
-                self.window.open_file(join(self.packages, settings[value][0]))
+    def get_zip_packages(self, file_path, package_type):
+        plugins = [(join(file_path, item), package_type) for item in listdir(file_path) if fnmatch(item, "*.sublime-package")]
+        return plugins
 
+    def search_zipped_files(self):
+        plugins = []
+        st_packages = sublime_package_paths()
+        plugins += self.get_zip_packages(st_packages[0], "Installed")
+        plugins += self.get_zip_packages(st_packages[1], "Default")
+        return plugins
+
+    def walk_zip(self, settings, plugin, pattern, regex):
+        with zipfile.ZipFile(plugin[0], 'r') as z:
+            zipped = [(join(basename(plugin[0]), normpath(fn)), plugin[1]) for fn in sorted(z.namelist())]
+            self.find_files(zipped, pattern, settings, regex)
+
+    def find_raw(self, pattern, regex=False):
+        self.packages = normpath(sublime.packages_path())
+        settings = []
+        plugins = [join(self.packages, item) for item in listdir(self.packages) if isdir(join(self.packages, item))]
+        for plugin in plugins:
+            self.walk(settings, plugin, pattern.strip(), regex)
+
+        self.zipped_idx = len(settings)
+
+        zipped_plugins = self.search_zipped_files()
+        for plugin in zipped_plugins:
+            self.walk_zip(settings, plugin, pattern.strip(), regex)
+
+        self.window.show_quick_panel(
+            settings,
+            lambda x: self.process_file(x, settings=settings)
+        )
+
+    def find(self, pattern, regex):
+        resources = []
+        if not regex:
+            resources = sublime.find_resources(pattern)
+        else:
+            temp = sublime.find_resources("*")
+            for t in temp:
+                if re.match(pattern, t, re.IGNORECASE) != None:
+                    resources.append(t)
+
+        self.window.show_quick_panel(
+            resources,
+            lambda x: self.process_file(x, settings=resources),
+            0,
+            0,
+            lambda x: self.on_select(x, settings=resources)
+        )
+
+    def run(self, **kwargs):
+        kwargs = self.pre_process(**kwargs)
+        pattern = kwargs.get("pattern", None)
+        regex = kwargs.get("regex", False)
+        self.find_all = kwargs.get("find_all", False)
+
+        if not self.find_all:
+            self.find(pattern, regex)
+        else:
+            self.find_raw(pattern, regex)
+
+
+class GetPackageFilesCommand(_PackageSearch):
     def open_zip_file(self, fn):
         file_name = None
         zip_package = None
@@ -226,59 +252,29 @@ class GetPackageFilesCommand(sublime_plugin.WindowCommand):
                 WriteArchivedPackageContentCommand.bfr = text.decode('utf-8').replace('\r', '')
                 sublime.set_timeout(lambda: view.run_command("write_archived_package_content"), 0)
 
+    def process_file(self, value, settings):
+        if value > -1:
+            if self.find_all:
+                if value >= self.zipped_idx:
+                    self.open_zip_file(settings[value][0])
+                else:
+                    self.window.open_file(join(self.packages, settings[value][0]))
+            else:
+                self.window.run_command("open_file", {"file": settings[value].replace("Packages", "${packages}", 1)})
 
-    def get_zip_packages(self, file_path, package_type):
-        plugins = [(join(file_path, item), package_type) for item in listdir(file_path) if fnmatch(item, "*.sublime-package")]
-        return plugins
 
-    def search_zipped_files(self):
-        plugins = []
-        st_packages = sublime_package_paths()
-        plugins += self.get_zip_packages(st_packages[0], "Installed")
-        plugins += self.get_zip_packages(st_packages[1], "Default")
-        return plugins
+class GetColorSchemeFileCommand(GetPackageFilesCommand):
+    def on_select(self, value, settings):
+        if value != -1:
+            sublime.load_settings("Preferences.sublime-settings").set("color_scheme", settings[value])
 
-    def walk_zip(self, settings, plugin, pattern, regex):
-        # psuedo_path = join(normpath(sublime.packages_path()), splitext(basename(plugin))[0])
-        with zipfile.ZipFile(plugin[0], 'r') as z:
-            zipped = [(join(basename(plugin[0]), normpath(fn)), plugin[1]) for fn in sorted(z.namelist())]
-            self.find_files(zipped, pattern, settings, regex)
-
-    def find_all(self, pattern, regex=False):
-        self.packages = normpath(sublime.packages_path())
-        settings = []
-        plugins = [join(self.packages, item) for item in listdir(self.packages) if isdir(join(self.packages, item))]
-        for plugin in plugins:
-            self.walk(settings, plugin, pattern.strip(), regex)
-
-        self.zipped_idx = len(settings)
-
-        zipped_plugins = self.search_zipped_files()
-        for plugin in zipped_plugins:
-            self.walk_zip(settings, plugin, pattern.strip(), regex)
-
-        self.window.show_quick_panel(
-            settings,
-            lambda x: self.open_file(x, settings=settings)
-        )
-
-    def find(self, pattern, regex):
-        resources = []
-        if not regex:
-            resources = sublime.find_resources(pattern)
+    def process_file(self, value, settings):
+        if value != -1:
+            sublime.load_settings("Preferences.sublime-settings").set("color_scheme", settings[value])
         else:
-            temp = sublime.find_resources("*")
-            for t in temp:
-                if re.match(pattern, t, re.IGNORECASE) != None:
-                    resources.append(t)
+            if self.current_color_scheme is not None:
+                sublime.load_settings("Preferences.sublime-settings").set("color_scheme", self.current_color_scheme)
 
-        self.window.show_quick_panel(
-            resources,
-            lambda x: self.open_file_quick(x, settings=resources)
-        )
-
-    def run(self, pattern, regex=False, find_all=False):
-        if not find_all:
-            self.find(pattern, regex)
-        else:
-            self.find_all(pattern, regex)
+    def pre_process(self, **kwargs):
+        self.current_color_scheme = sublime.load_settings("Preferences.sublime-settings").get("color_scheme")
+        return {"pattern": "*.tmTheme"}
